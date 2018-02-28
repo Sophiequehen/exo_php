93684d3ed358a12eeb90170fc1ba6451dbf9e2cc
Sophiequehen <sophie.quehen@gmail.com>
2018-02-28 12:31:58 +0100

    changement de la page evenements selon si on est connectés ou non, ajout d'image pour la page evenements

--- menu.php
+++ menu.php
@@ -2,24 +2,28 @@
 	<li>
 		<?php 
 		if(!empty($_SESSION['user'])){
-			echo "Bonjour"." ".$_SESSION['user'];
+			echo "Bonjour"." ".$_SESSION['user']." "."!";
 		}
-			?>
-
-			
+		?>			
 	</li>
 	<li><a href="home.php">HOME</a></li>
 	<li><a href="apropos.php">À PROPOS</a></li>
 	<li><a href="contact.php">CONTACT</a></li>
-	<li><a href="connexion.php">ÉVÈNEMENTS</a></li>
+	<li>
+		<?php 
+		if(!empty($_SESSION['user'])){
+			echo '<a href="evenements.php">ÉVÈNEMENTS</a>';
+		}else{
+			echo '<a href="connexion.php">ÉVÈNEMENTS</a>';
+		}
+		?>	
+	</li>
 	<li><a href="blog.php">BLOG</a></li>
-	<li><a href="inscription.php">LOGIN</a></li>
-
-	
+	<li><a href="inscription.php">LOGIN</a></li>	
 	<?php
 	if(!empty($_SESSION['user'])){
-			echo '<li><a href="deconnexion.php">Déconnexion</a></li>';
-		}
-		?>
+		echo '<li><a href="deconnexion.php">Déconnexion</a></li>';
+	}
+	?>
 </ul>
 
